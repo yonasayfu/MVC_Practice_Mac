449515480754e55d48ca23a1db68910e1f9ce5c8
Yonas <yonasayfu28@gmail.com>
2023-09-03 20:45:33 +0300

    Project MVC Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Employee_Mac.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
